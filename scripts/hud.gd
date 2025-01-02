6cd6f3b001098ada19acb1266b66f1e347a079bc
Anastasiia Tatlubaeva <tatlubaevaa@gmail.com>
2025-01-01 23:17:30 -0500

    first commit

---
+++ scripts/hud.gd
@@ -0,0 +1,44 @@
+extends CanvasLayer
+
+signal start_game  # notify Main scene that the button has been pressed
+var game_started = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("start_game") and not game_started:
+		$StartButton.hide()
+		start_game.emit()
+		game_started = true
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+	
+
+func show_game_over():
+	show_message("Game Over")
+	await $MessageTimer.timeout
+	
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+	
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+	game_started = false
+	
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+func _on_start_button_pressed() -> void:
+	$StartButton.hide()
+	start_game.emit()
+	game_started = true
+
+func _on_message_timer_timeout() -> void:
+	$Message.hide()
---
+++ scripts/main.gd
@@ -0,0 +1,82 @@
+extends Node
+
+@export var mob_scene: PackedScene
+@export var coin_scene: PackedScene
+var score
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func game_over() -> void:
+	$Music.stop()
+	$DeathSound.play()
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$CoinTimer.stop()
+	
+	get_tree().call_group("mobs", "queue_free")
+	get_tree().call_group("coins", "queue_free")
+	
+	$HUD.show_game_over()
+	
+func coin_collected() -> void:
+	$CoinSound.play()
+	score += 5
+	$HUD.update_score(score)
+	
+func new_game():
+	score = 0
+	$Music.play()
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
+
+func _on_mob_timer_timeout() -> void:
+	var mob = mob_scene.instantiate()  # create a new instance of mob scene
+	
+	var mob_spawn_location = $MobPath/MobSpawnLocation  # choose random location on Path2D
+	mob_spawn_location.progress_ratio = randf()
+	
+	var direction = mob_spawn_location.rotation + PI / 2  # set mob's direction perpendicular to path direction
+	
+	mob.position = mob_spawn_location.position  # set mob's position to random location
+	
+	direction += randf_range(-PI / 4, PI / 4)  
+	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)  # add mob to main scene - spawn it
+
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+	$HUD.update_score(score)
+
+func _on_start_timer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
+	$CoinTimer.start()
+
+func _on_coin_timer_timeout() -> void:
+	var coin = coin_scene.instantiate()
+	
+	var screen_size = get_viewport().get_visible_rect().size
+	var buffer = 50  # Distance from the edges
+	
+	var random_x = randf_range(buffer, screen_size.x - buffer)
+	var random_y = randf_range(buffer, screen_size.y - buffer)
+	coin.position = Vector2(random_x, random_y)
+	
+	add_child(coin)
+	
